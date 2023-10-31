7c487e424603e877ff961566108cca1bb37eb52d
Qasim <qasimyousaf094@gmail.com>
2023-10-31 23:08:53 +0500

    first commit

--- Projects/my-app/app/page.js
+++ Projects/my-app/app/page.js
@@ -1,10 +1,10 @@
-import Image from 'next/image' ;
-import Navbar from './(components)/navbar/navbar';
-import Footer from './(components)/footer/footer';
+
 
 export default function Home() {
+
   return (
- <div>
+ <div className="text-"> 
+
 
 
 </div>
