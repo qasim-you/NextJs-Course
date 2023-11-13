35571dc74be37ef05d2b14576fa370882a81da9c
Qasim <qasimyousaf094@gmail.com>
2023-11-13 22:45:49 +0500

    first commit

---
+++ Projects/powwer/app/page.js
@@ -0,0 +1,11 @@
+
+
+export default function App() {
+  return (
+    <div className="flex flex-1 justify-center">
+      <h1 className="placeholder-lime-400 py-14">
+        Qasim 
+      </h1>
+    </div>
+  )
+}
