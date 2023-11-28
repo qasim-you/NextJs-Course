1bb254b60fdb74aa84ee295d76ed1618ae25e5d6
Qasim <qasimyousaf094@gmail.com>
2023-11-28 23:06:30 +0500

    first commit

--- Projects/database-app/app/page.js
+++ Projects/database-app/app/page.js
@@ -1,4 +1,4 @@
-
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -87,7 +87,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+            Explore starter templates for Next.js.
           </p>
         </a>
 
